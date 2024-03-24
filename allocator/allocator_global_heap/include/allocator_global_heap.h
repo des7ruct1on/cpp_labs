8b321849ef9b3c22e54d4f993263788ae09945af
shad0w777 <shad0w2020@mail.ru>
2024-03-25 00:59:31 +0300

    fixed ebuchiy vneshniy vid code defaultnogo

--- allocator/allocator_global_heap/include/allocator_global_heap.h
+++ allocator/allocator_global_heap/include/allocator_global_heap.h
@@ -6,58 +6,35 @@
 #include <logger_guardant.h>
 #include <typename_holder.h>
 
-class allocator_global_heap final:
-    public allocator,
-    private logger_guardant,
-    private typename_holder
-{
+class allocator_global_heap final: public allocator, private logger_guardant, private typename_holder {
 
 private:
     
     logger *_logger;
 
-public:
-    
-    explicit allocator_global_heap(
-        logger *logger = nullptr);
-    
-    ~allocator_global_heap() override;
-    
-    allocator_global_heap(
-        allocator_global_heap const &other) = delete;
-    
-    allocator_global_heap &operator=(
-        allocator_global_heap const &other) = delete;
-    
-    allocator_global_heap(
-        allocator_global_heap &&other) noexcept;
-    
-    allocator_global_heap &operator=(
-        allocator_global_heap &&other) noexcept;
-
-public:
-    
-    [[nodiscard]] void *allocate(
-        size_t value_size,
-        size_t values_count) override;
-    
-    void deallocate(
-        void *at) override;
-
-public:
-    
-    void foo()
-    {};
-
-private:
-    
     inline logger *get_logger() const override;
 
-private:
-    
     inline std::string get_typename() const noexcept override;
 
 public:
+    
+    explicit allocator_global_heap(logger *logger = nullptr);
+    
+    ~allocator_global_heap() override;
+    
+    allocator_global_heap(allocator_global_heap const &other) = delete;
+    
+    allocator_global_heap &operator=(allocator_global_heap const &other) = delete;
+    
+    allocator_global_heap(allocator_global_heap &&other) noexcept;
+    
+    allocator_global_heap &operator=(allocator_global_heap &&other) noexcept;
+
+    [[nodiscard]] void *allocate(size_t value_size, size_t values_count) override;
+    
+    void deallocate(void *at) override;
+    
+    void foo() {};
 
 };
 
