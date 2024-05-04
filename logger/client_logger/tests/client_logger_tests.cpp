61efb0d75c9cdeb65bd7012b990d38f83064213b
shad0w777 <shad0w2020@mail.ru>
2024-05-04 11:52:10 +0300

    added tests

--- logger/client_logger/tests/client_logger_tests.cpp
+++ logger/client_logger/tests/client_logger_tests.cpp
@@ -3,6 +3,42 @@
 #include <client_logger_builder.h>
 #include <fstream>
 
+
+TEST(ClientLoggerTest, LogToFileAndConsole) {
+    logger_builder* builder = new client_logger_builder();
+
+    logger* logger_tmp = builder
+            ->add_file_stream("file_1.txt", logger::severity::information)
+            ->add_file_stream("file_2.txt", logger::severity::debug)
+            ->add_console_stream(logger::severity::debug)
+            ->build();
+
+    logger_tmp->log("Test message 1", logger::severity::debug);
+    logger_tmp->log("Test message 2", logger::severity::information);
+    logger_tmp->log("Test message 3", logger::severity::warning);
+    logger_tmp->log("Test message 4", logger::severity::error);
+
+    delete builder;
+    delete logger_tmp;
+
+    std::ifstream file1("file_1.txt");
+    std::ifstream file2("file_2.txt");
+    std::string line;
+    int count = 0;
+    while (std::getline(file1, line)) {
+        count++;
+    }
+    while (std::getline(file2, line)) {
+        count++;
+    }
+    file1.close();
+    file2.close();
+    ASSERT_EQ(count, 4); 
+
+    std::cout << "Check console output for logged messages\n";
+}
+
+
 int main(
     int argc,
     char *argv[])
